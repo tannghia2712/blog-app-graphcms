fb4c91712fca9d569cc7a5ddb0104734491a4248
tannghia <84166584+tannghia2712@users.noreply.github.com>
2022-09-28 18:44:53 +0700

    update

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -53,7 +53,7 @@ const CommentsForm = ({ slug }) => {
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">CommentsForm</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a comment</h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea ref={commentEl} className="p-4 focus:ring-2  focus:ring-gray-200 outline-none w-full rounded-lg  bg-gray-100 text-gray-700" placeholder="Comment" name="comment">
         </textarea>
